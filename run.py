36833e28ce802024608d2be7863fc3a7178977a3
hugoht3 <hugoht3@hotmail.com>
2024-05-15 11:42:51 +0100

    first commit for saving purpose

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+##Started with a battle ship game
+
+
+##First functions are just pirnting something to the terminal to see if they are working.
+
+##To se more about the project check the video of the pyhthon 3 project.
+
+
+# Your code goes here.
+# You can delete these comments, but do not change the name of this file
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+score = {"player": 0, "computer": 0}
+
+def main():
+    '''
+    The function wich will start all other functions attached to the game
+
+    '''
+    
+    
+    print('Welcome to XXXXXXX')
+    player_name = input("Plase insert your name: \n")
+    #get_non_numeric_input(player_name)
+
+
+#def get_non_numeric_input(player_name):
+      #  '''
+       # Function that makes the name not to have numbers
+       # '''
+       # while True:
+      #      value = input(player_name)
+      #  if not any(char.isdigit() for char in value):
+       #     return value            
+      #  print("Input cannot contain numbers. Please try again.")
+
+
+
+
+main()
