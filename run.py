f1fd282436904f48e5a214d0634c038498ff3936
hugoht3 <hugoht3@hotmail.com>
2024-05-16 10:16:46 +0100

    created a function that sets a board

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 ##Started with a battle ship game
 
 
@@ -9,35 +8,24 @@
 ##To se more about the project check the video of the pyhthon 3 project.
 
 
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 score = {"player": 0, "computer": 0}
 
 def main():
     '''
     The function wich will start all other functions attached to the game
-
     '''
-    
+    size = 5
+    board = create_board(size)
     
     print('Welcome to XXXXXXX')
     player_name = input("Plase insert your name: \n")
     #get_non_numeric_input(player_name)
 
-
-#def get_non_numeric_input(player_name):
-      #  '''
-       # Function that makes the name not to have numbers
-       # '''
-       # while True:
-      #      value = input(player_name)
-      #  if not any(char.isdigit() for char in value):
-       #     return value            
-      #  print("Input cannot contain numbers. Please try again.")
+def create_board(size):
+    return [['O'] * size for i in range(size)]
 
 
 
 
-main()
+board = create_board(5)
+print(board)
